50d382fec1648a43c1dabcc34f552e5aced98d58
Newman <nslanier@yahoo.com>
2025-11-12 18:02:28 -0500

    WIP - trying to fix the export static force-static issues with the add member API - but I don't need the add member in the exported file, just in the run dev

--- next.config.js
+++ next.config.js
@@ -1,14 +1,8 @@
-/**
- * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
- * for Docker builds.
- */
-import "./src/env.js";
-
-/** @type {import("next").NextConfig} */
+import 'dotenv/config';
+const isExport = process.env.EXPORT_MODE === 'true';
 const config = {
-    output: 'export', // important for static hosting
-    basePath: '/Global-Presence-Map', // your repo name
-    assetPrefix: '/Global-Presence-Map/', // ensures assets load correctly
+    basePath: '/Global-Presence-Map',
+    assetPrefix: '/Global-Presence-Map/',
+    ...(isExport ? { output: 'export' } : {}),
 };
-
 export default config;
